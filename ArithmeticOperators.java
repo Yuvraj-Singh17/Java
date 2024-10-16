4fbde8204e7205c67f7c9217c1a20cb07dc3604b
Yuvraj-Singh17 <yuvivishu17@gmail.com>
2024-10-16 23:38:27 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,23 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        //Arithmetic operators --> + , - , * , / , %
+        int a = 100;
+        int b = 10;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);//it gives remainder
+        a += b;          //We can do like this also it means a = a + b
+        System.out.println(a);
+        a -= b;          //it means a = a - b
+        System.out.println(a);
+        a *= b;          //it means a = a * b
+        System.out.println(a);
+        a /= b;          //it means a = a / b
+        System.out.println(a);
+        a %= b;          //it means a = a % b
+        System.out.println(a);
+        
+    }
+}
